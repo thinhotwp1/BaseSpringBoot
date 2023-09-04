6348c58a13a63361eb106f4e032c39eb98208612
ThinhLD <leduythinh20001@gmail.com>
2023-09-04 11:01:26 +0700

    init

---
+++ BaseSpringBoot/src/main/java/com/example/basespringboot/BaseSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.basespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseSpringBootApplication.class, args);
+	}
+
+}
---
+++ BaseSpringBoot/src/test/java/com/example/basespringboot/BaseSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.basespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
