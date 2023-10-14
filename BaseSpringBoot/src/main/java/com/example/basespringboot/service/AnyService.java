e48dd4221c27c17fd3a77acfa3a745d023055f97
ThinhLD <leduythinh20001@gmail.com>
2023-10-14 11:54:53 +0700

    update callRestAPI

--- BaseSpringBoot/src/main/java/com/example/basespringboot/service/AnyService.java
+++ BaseSpringBoot/src/main/java/com/example/basespringboot/service/AnyService.java
@@ -1,12 +1,23 @@
 package com.example.basespringboot.service;
 
 import com.example.basespringboot.rest.ResponseData;
+import com.example.basespringboot.rest.RestClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AnyService {
+
+    @Autowired
+    RestClient restClient;
+
     public ResponseData<?> getName(int number) {
         return new ResponseData<>().success(number);
+    }
+
+    public Object callRestAPI(Object request) throws Exception {
+        restClient.setUrl("https://path-you-want");
+        return restClient.postObject(request,Object.class);
     }
 }
